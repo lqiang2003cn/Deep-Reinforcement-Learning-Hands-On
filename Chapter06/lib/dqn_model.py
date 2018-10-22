289dbde68b44756a4cdc41ac1b837126b4ab0757
lqiang2003cn <randycoq@163.com>
2018-10-22 16:08:50 +0800

    11

--- Chapter06/lib/dqn_model.py
+++ Chapter06/lib/dqn_model.py
@@ -24,10 +24,14 @@ class DQN(nn.Module):
             nn.Linear(512, n_actions)
         )
 
+    #如果不用这个，则需要手动计算卷积网络的输出；
     def _get_conv_out(self, shape):
         o = self.conv(torch.zeros(1, *shape))
         return int(np.prod(o.size()))
 
+    #所有的向前传递的逻辑都必须实现在这个方法里面，默认的实现就是放入一个nn。这里实现了两个nn的连接
     def forward(self, x):
+        #1、先将输入参数传入到卷积nn，将得到的结果变成一维；
+        #2、将一维的输出作为输入，传入到全联网络；
         conv_out = self.conv(x).view(x.size()[0], -1)
         return self.fc(conv_out)
